23551394542973db9ea7715f75d4c24e3c2b7225
abel-TI23 <abel.hendrik_ti23@nusaputra.ac.id>
2025-06-17 16:48:17 +0700

    Update appMLPP.py

--- appMLPP.py
+++ appMLPP.py
@@ -131,7 +131,7 @@ if ticker:
                         close=df['Close'],
                         name='Harga Aktual',
                         ### PERUBAHAN 1: Mengubah warna candle
-                        increasing=dict(line=dict(color='grey')), 
+                        increasing=dict(line=dict(color='green')), 
                         decreasing=dict(line=dict(color='red'))
                         ))
 
