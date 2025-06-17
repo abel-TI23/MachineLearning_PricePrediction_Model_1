61a9bbd3b9bd78d35928581d94dbbf1fb77ac552
abel-TI23 <abel.hendrik_ti23@nusaputra.ac.id>
2025-06-17 15:57:40 +0700

    Update appMLPP.py

--- appMLPP.py
+++ appMLPP.py
@@ -83,7 +83,7 @@ def load_artifacts():
 
 # === Streamlit App ===
 st.set_page_config(page_title="Predictive Price Model", layout="wide")
-st.title("📈 Predict the losing Price - Streamlit")
+st.title("📈 Predict the Closing Price - Streamlit")
 
 with st.sidebar:
     st.header("Pengaturan")
